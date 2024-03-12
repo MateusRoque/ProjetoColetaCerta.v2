6c3cc128c293aac0cbe9a6d4f3ebf1c56d3811b2
Mateus Roque <mateusroque1406@gmail.com>
2024-03-12 15:27:50 -0300

    Creating the first page of app, putting the logo

---
+++ screens/welcomepage.js
@@ -0,0 +1,70 @@
+// FirstScreen.js
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
+export function FirstScreen() {
+  const navigation = useNavigation();
+
+  const goToNextScreen = () => {
+    navigation.navigate('a');
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={require("../src/assets/logo-sem-fundo-red.png")} style={styles.logo} resizeMode="contain"/>
+      </View>
+      <View style={styles.paragraphContainer}>
+        <Text style={styles.paragraph}>Parabéns! Você está prestes a colocar a "tal sustentabilidade" na prática.</Text>
+
+        <Text style={styles.paragraph}>A ColetaCerta tem como objetivo
+        conectar você com o município. Notificamos sobre o horário da coleta seletiva e também recebemos reclamações do município, sejam elas desobstrução de bueiros, etc...
+        </Text>
+        
+        <TouchableOpacity onPress={goToNextScreen}>
+          <Text style={styles.paragraphLink}>
+            Clique aqui para começar
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+ 
+    )};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+
+
+  }, 
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    backgroundColor: "blue",
+  },
+  paragraphContainer: {
+    flex: 0.6,
+    alignItems: "center",
+    rowGap: 30,
+    justifyContent: "center",
+    
+  },
+  paragraph: {
+    fontSize: 18,
+    width:300,
+    textAlign: 'justify',
+
+  },
+  paragraphLink: {
+    fontSize: 19,
+    fontWeight: "bold",
+    color: "green"
+
+  }
+
+})
+export default FirstScreen;
